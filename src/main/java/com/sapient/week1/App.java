07bb07cfb4c9d640993d6ed1973450c340d22428
chandanreddy <chandanreddybanda@gmail.com>
2020-04-09 20:52:51 +0530

    first Commit

---
+++ src/main/java/com/sapient/week1/App.java
@@ -0,0 +1,21 @@
+package com.sapient.week1;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args){}
+    	public App(){
+        
+    	}
+    
+	public Boolean check(int n) {
+		if(n%2==0){
+	 	   return true;
+		}
+		else{
+	  	   return false;
+		}
+    	} 
+}

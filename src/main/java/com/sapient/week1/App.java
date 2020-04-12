162af336a5283c6b3961ec3ed57144f1ce3457e5
Chandan Reddy <42784644+chandanreddybanda@users.noreply.github.com>
2020-04-12 18:49:52 +0530

    Update App.java

--- src/main/java/com/sapient/week1/App.java
+++ src/main/java/com/sapient/week1/App.java
@@ -5,7 +5,7 @@ package com.sapient.week1;
  */
 public class App 
 {
-	public static void main(String[] args){}
+	public static void main(String[] args){System.out.println("Done Properly");}
     	public App(){
         
     	}

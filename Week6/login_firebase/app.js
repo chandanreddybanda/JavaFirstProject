bcf483c3d207ec5c0504b04bc7bd44b832deea03
Chandan Reddy <42784644+chandanreddybanda@users.noreply.github.com>
2020-05-20 02:04:17 +0530

    Add files via upload

---
+++ Week6/login_firebase/app.js
@@ -0,0 +1,13 @@
+
+ 
+    const config = {
+        apiKey: "AIzaSyDK7BWCSdKuo-KapcNeNjuB-wIZSgr70qk",
+        authDomain: "secondprojecttt.firebaseapp.com",
+        databaseURL: "https://secondprojecttt.firebaseio.com",
+        projectId: "secondprojecttt",
+        storageBucket: "secondprojecttt.appspot.com",
+        messagingSenderId: "717849475281",
+        appId: "1:717849475281:web:fc8be3c2ec64568cb62df0",
+        measurementId: "G-M54QGYJ68H"
+    };
+    firebase.initializeApp(config);
---
+++ Week6/login_firebase/login.js
@@ -0,0 +1,36 @@
+var x = 0;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+  
+      //document.getElementById("user_div").style.display = "block";
+      //document.getElementById("login_div").style.display = "none";
+    } else {
+      //document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+      
+    }
+  });
+  
+  function login(){
+  
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      window.alert("Error : " + errorMessage);
+      
+      // ...
+    });
+    window.location="index.html";
+  }
+  
+  function logout(){
+    firebase.auth().signOut();
+    window.location="login.html";
+  }
+  
